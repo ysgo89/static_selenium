f0b95acda481e4fb1209cb75933c3d00e3ded8e1
goyoseb <goyoseb@suresofttech.com>
2019-08-06 12:21:48 +0900

    - 신규 TC 추가 : C18694 - 주석 내용 변경 : C18693

--- Test_Suite/C18694.py
+++ Test_Suite/C18694.py
@@ -3,20 +3,20 @@ import unittest
 import time
 
 # Test Case_id 정보
-case_id = 18693
+case_id = 30722
 
-class C18693(unittest.TestCase):
-    def test_C18693(self):
+class C18694(unittest.TestCase):
+    def test_C18694(self):
         # default_setting 수행
         p: default = default()
         p.setUp()
 
-        # STATIC 접속 -> User 필드에 값을 넣지 않고 회원가입 시도
+        # STATIC 접속 -> Password 필드에 값을 입력하지 않고 회원가입 시도
         p.driver.get(address)
         p.driver.find_element_by_link_text("Create account").click()
-        p.driver.find_element_by_id("username").send_keys("")
+        p.driver.find_element_by_id("username").send_keys("goyoseb123")
         p.driver.find_element_by_id("email").send_keys("admin123@static.ioi")
-        p.driver.find_element_by_id("password").send_keys("1234567")
+        p.driver.find_element_by_id("password").send_keys("")
         p.driver.find_element_by_xpath("//button").click()
         time.sleep(1)
 
@@ -25,7 +25,7 @@ class C18693(unittest.TestCase):
         # TestRail 결과 입력
         try :
             # Validate 문구 확인
-            self.assertEqual(valCheck, p.driver.find_element_by_xpath("//small").text)
+            self.assertEqual(valCheck, p.driver.find_element_by_xpath("//div[3]/small").text)
             self.assertFalse(p.driver.find_element_by_xpath("//button").is_enabled())
             status_id = 1
         except :
